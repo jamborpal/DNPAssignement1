4dffa5214908de3da977e91930363e4f446abfe0
Pál Jámbor <jamborpal0@gmail.com>
2020-10-19 12:19:40 +0200

    FINAL

--- Login/User.cs
+++ Login/User.cs
@@ -1,9 +1,15 @@
-﻿namespace Assignment1.Login
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Assignment1.Login
 {
     public class User
     {
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+
     }
 }
